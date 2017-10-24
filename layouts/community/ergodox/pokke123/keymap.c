bdf51237dfe372d2aff9ead2e8c9db75ae76b060
adriaankoman@gmail.com <adriaankoman@gmail.com>
2017-10-24 10:18:11 +0200

    Add tap togle to layer

--- layouts/community/ergodox/pokke123/keymap.c
+++ layouts/community/ergodox/pokke123/keymap.c
@@ -5,6 +5,7 @@
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
+#define TAPPING_TOGGLE 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
@@ -33,9 +34,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
   // left hand
   KC_ESC,     KC_1,    KC_2,     KC_3,     KC_4,               KC_5,  KC_F5,
-  KC_TAB,     KC_Q,    KC_W,     KC_E,     KC_R,               KC_T,  MO(2),
+  KC_TAB,     KC_Q,    KC_W,     KC_E,     KC_R,               KC_T,  TT(2),
   KC_LCTL,    KC_A,    KC_S,     KC_D,     KC_F,               KC_G,
-  KC_LSFT,    KC_Z,    KC_X,     KC_C,     KC_V,               KC_B,  MO(1),
+  KC_LSFT,    KC_Z,    KC_X,     KC_C,     KC_V,               KC_B,  TT(1),
   MO(1),      KC_GRV,  KC_QUOT,  KC_BSLS,  MO(2),
 
                                                KC_HOME,   KC_END,
@@ -44,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // right hand
   KC_ESC,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,      KC_BSPC,
-  MO(2),   KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,      KC_BSLS,
+  TT(2),   KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,      KC_BSLS,
            KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,   KC_QUOT,
-  MO(1),   KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,   KC_MINS,
+  TT(1),   KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,   KC_MINS,
                      KC_LCBR,  KC_RCBR,  KC_LBRC,  KC_RBRC,   MO(1),
 
   KC_PGDOWN,  KC_INS,
