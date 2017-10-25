7cc34c56b36f939418797141bb5502ebd438b69f
adriaankoman@gmail.com <adriaankoman@gmail.com>
2017-10-25 16:57:48 +0200

    reorderd mouse buttons

--- layouts/community/ergodox/pokke123/keymap.c
+++ layouts/community/ergodox/pokke123/keymap.c
@@ -119,7 +119,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // MEDIA AND MOUSE
 LAYOUT_ergodox(
          RESET, KC_TRNS,        KC_TRNS,     KC_TRNS,        KC_TRNS,       KC_TRNS,    KC_TRNS,
-       KC_TRNS, KC_TRNS,        KC_TRNS,     KC_MS_UP,       KC_BTN1,       KC_TRNS,    KC_TRNS,
+       KC_TRNS, KC_TRNS,        KC_BTN1,     KC_MS_UP,       KC_BTN2,       KC_TRNS,    KC_TRNS,
        KC_TRNS, KC_TRNS,        KC_MS_LEFT,  KC_MS_DOWN,     KC_MS_RIGHT,   KC_TRNS,
        KC_TRNS, KC_TRNS,        KC_TRNS,     KC_TRNS,        KC_TRNS,       KC_TRNS,    KC_TRNS,
        KC_TRNS, KC_TRNS,        KC_TRNS,     KC_TRNS,        KC_TRNS,
